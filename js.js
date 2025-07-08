e0a789c45d9eaa3cf9a7a39de0c43607abffdf7d
arpit <arpitjamwal000@gmail.com>
2025-07-08 19:27:54 +0530

    add

---
+++ js.js
@@ -0,0 +1,8 @@
+const show=()=>{
+    const show=document.querySelector('.hidebar')
+    show.style.display='flex'
+}
+const hide=()=>{
+    const show=document.querySelector('.hidebar')
+    show.style.display='none'
+}
